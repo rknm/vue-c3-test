1b085f1af413f64ca4a711696fbb9521f244ef63
RK <rk@RK-PC.localdomain>
2020-01-29 01:07:21 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Chart :data="data"></Chart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Chart from "./components/Chart.vue";
+
+@Component({
+  components: {
+    Chart
+  }
+})
+export default class App extends Vue {
+  data = [
+    { key: "data1", count: 500 },
+    { key: "data2", count: 250 },
+    { key: "data3", count: 150 },
+    { key: "data4", count: 100 }
+  ];
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="chart"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import c3 from "c3";
+
+@Component
+export default class Chart extends Vue {
+  @Prop()
+  data!: { key: string; count: number }[];
+
+  mounted() {
+    c3.generate({
+      bindto: "#chart",
+      data: {
+        columns: this.data.map(value => [value.key, value.count]),
+        type: "donut"
+      }
+    });
+  }
+}
+</script>
+
+<style scoped>
+@import "~c3/c3.min.css";
+</style>
